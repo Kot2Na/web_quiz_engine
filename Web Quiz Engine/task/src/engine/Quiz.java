1508f18c3543ad25e3d32b9e544fd9a6e7da596c
Kot2Na <nikitin.na@phystech.edu>
2021-04-16 10:18:36 +0300

    Stage 4

--- Web Quiz Engine/task/src/engine/Quiz.java
+++ Web Quiz Engine/task/src/engine/Quiz.java
@@ -1,32 +1,40 @@
 package engine;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "Quiz")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Quiz {
+    @Id
+    @GeneratedValue
+    @Column(name = "Id", nullable = false)
     private int id;
 
     @NotBlank
+    @Column(name = "Title")
     private String title;
 
     @NotBlank
+    @Column(name = "Text")
     private String text;
 
     @Size(min = 2)
+    @Column(name = "Options")
     @NotNull(message = "Options mustn't be null.")
     private String[] options;
 
-
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "Answer")
     private Integer[] answer;
 }
--- Web Quiz Engine/task/src/engine/QuizRestController.java
+++ Web Quiz Engine/task/src/engine/QuizRestController.java
@@ -1,6 +1,7 @@
 package engine;
 
 import org.apache.tomcat.util.digester.ArrayStack;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
@@ -11,26 +12,33 @@ import java.util.*;
 
 @RestController
 public class QuizRestController {
-    private Map<Integer, Quiz> map = new HashMap<Integer, Quiz>();
+    //private Map<Integer, Quiz> map = new HashMap<Integer, Quiz>();
+    @Autowired
+    private QuizRepository quizRepository;
 
     @PostMapping("/api/quizzes")
     public Quiz putQuiz(@Valid @RequestBody Quiz quiz) {
         Quiz newQuiz;
+        System.out.println(this.quizRepository.equals(null));
         if (quiz.getAnswer() != null) {
-            newQuiz = new Quiz(map.size() + 1, quiz.getTitle(), quiz.getText(), quiz.getOptions(), quiz.getAnswer());
+            newQuiz = new Quiz((int) this.quizRepository.count() + 1, quiz.getTitle(), quiz.getText(), quiz.getOptions(), quiz.getAnswer());
         }
         else {
-            newQuiz = new Quiz(map.size() + 1, quiz.getTitle(), quiz.getText(), quiz.getOptions(), new Integer[]{});
+            newQuiz = new Quiz((int) this.quizRepository.count() + 1, quiz.getTitle(), quiz.getText(), quiz.getOptions(), new Integer[]{});
         }
-        this.map.put(newQuiz.getId(), newQuiz);
+        this.quizRepository.save(newQuiz);
+
+        //this.map.put(newQuiz.getId(), newQuiz);
 
         return newQuiz;
     }
 
     @GetMapping("/api/quizzes/{id}")
     public Quiz getQuiz(@PathVariable int id) {
-        if (this.map.containsKey(id)) {
-            Quiz quiz = this.map.get(id);
+        //if (this.map.containsKey(id)) {
+        if (quizRepository.existsById(id)) {
+            Quiz quiz = quizRepository.findById(id).get();
+            //Quiz quiz = this.map.get(id);
             Quiz returnQuiz = new Quiz(id, quiz.getTitle(), quiz.getText(), quiz.getOptions(), quiz.getAnswer());
             return returnQuiz;
         } else {
@@ -42,12 +50,24 @@ public class QuizRestController {
     public ArrayList<Quiz> getQuizzes() {
         ArrayList<Quiz> quizArr = new ArrayList<Quiz>();
 
-        this.map.forEach((key, value) -> quizArr.add(new Quiz(key, value.getTitle(), value.getText(), value.getOptions(), value.getAnswer())));
+        quizRepository.findAll().forEach(value -> quizArr.add(new Quiz(value.getId(), value.getTitle(), value.getText(), value.getOptions(), value.getAnswer())));
+        //this.map.forEach((key, value) -> quizArr.add(new Quiz(key, value.getTitle(), value.getText(), value.getOptions(), value.getAnswer())));
         return quizArr;
     }
 
     @PostMapping("/api/quizzes/{id}/solve")
     public ResponseQuiz checkAnswerQuiz(@RequestBody Answer answer, @PathVariable int id) {
+        if (quizRepository.existsById(id)) {
+            if (Set.of(quizRepository.findById(id).get().getAnswer()).equals(Set.of(answer.getAnswer()))) {
+                return new ResponseQuiz(true, "Congratulations, you're right!");
+            } else {
+                return new ResponseQuiz(false, "Wrong answer! Please, try again.");
+            }
+        } else {
+            throw new QuizNotFound();
+        }
+
+        /*
         if (this.map.containsKey(id)) {
             if (Set.of(this.map.get(id).getAnswer()).equals(Set.of(answer.getAnswer()))) {
                 return new ResponseQuiz(true, "Congratulations, you're right!");
@@ -57,6 +77,7 @@ public class QuizRestController {
         } else  {
             throw new QuizNotFound();
         }
+        */
     }
 
     @ExceptionHandler(QuizNotFound.class)
@@ -67,5 +88,9 @@ public class QuizRestController {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public void handleValidationExceptions() {
+    }
+
+    public void setQuizRepository(QuizRepository quizRepository) {
+        this.quizRepository = quizRepository;
     }
 }
